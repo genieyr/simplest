de1d8ff14112187f8071ba7fd6d510c36022f653
Ruslan <ruslan.yatsevich@gmail.com>
2015-03-05 00:08:37 +0300

    https

---
+++ src/main/java/by/mtbank/ParamResource.java
@@ -0,0 +1,17 @@
+package by.mtbank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("presource/{id}")
+public class ParamResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String showParam(@PathParam("id") int i) {
+        return "ID is " + i;
+    }
+}
