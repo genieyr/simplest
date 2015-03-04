474957a6ff858ae5cdb42051ad8be503cb5546ce
Ruslan <ruslan.yatsevich@gmail.com>
2015-03-04 07:35:35 +0300

    1

---
+++ r.bat
@@ -0,0 +1,3 @@
+mvn archetype:generate -DarchetypeArtifactId=jersey-quickstart-webapp -DarchetypeGroupId=org.glassfish.jersey.archetypes -DinteractiveMode=false  -DgroupId=by.mtbank -DartifactId=simple-service-webapp -Dpackage=by.mtbank  -DarchetypeVersion=2.16
+rem mvn -Dmaven.test.skip=true
+rem http://www.itworld.com/article/2769041/development/how-to-configure-tomcat-to-always-require-https.html
