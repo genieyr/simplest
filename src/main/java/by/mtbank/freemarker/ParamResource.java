8a60373c573586b50deebcc00821febc77eef883
Ruslan <ruslan.yatsevich@gmail.com>
2015-03-20 10:37:59 +0300

    securityContext

---
+++ src/main/java/by/mtbank/freemarker/ParamResource.java
@@ -0,0 +1,19 @@
+package by.mtbank.freemarker;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("presource/{id}")
+public class ParamResource {
+
+    @GET
+//    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces(MediaType.APPLICATION_JSON)
+    public String showParam(@PathParam("id") int i) {
+        return "ID is " + i;
+    }
+}
